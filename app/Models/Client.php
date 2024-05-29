60dfc95c34756275839f22317ffff1e31ae79731
jimi24 <jimi@itbss.ac.id>
2024-05-29 09:44:53 +0700

    buat models

--- app/Models/Client.php
+++ app/Models/Client.php
@@ -8,4 +8,25 @@ use Illuminate\Database\Eloquent\Model;
 class Client extends Model
 {
     use HasFactory;
+
+    public $table = 'client';
+
+    const CREATE_AT = 'create_at';
+    const UPDATED_AT = 'updated_at';
+
+    protected $auditTimestamp = true;
+
+    public $fillable = [
+        'id',
+        'nama',
+        'alamat',
+        'no_telp',
+        'status',
+        'paket',
+        'kebutuhan',
+        'masa_aktif',
+        'nama_pic',
+        'telp_pic',
+        'alamat_pic'
+    ];
 }
