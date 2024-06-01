905092b12286955c419c09ec78dc9b39c5557aa3
dafon <danie.fonseca@itbss.ac.id>
2024-06-01 20:22:10 +0700

    perubahan web.php

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('index', function () {
     return view('index');
 });
+Route::get('/', function () {
+    return view('index');
+});
 Route::get('formclient', function () {
     return view('formclient');
 });
