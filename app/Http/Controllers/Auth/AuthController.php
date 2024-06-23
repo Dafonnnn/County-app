b908c5bb61c82a2780d081b6296d74f33f65db7e
Julius Kayne <juliuskayne07@gmail.com>
2024-06-23 20:56:00 +0700

    update(login): login

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,40 @@
+<?php
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Validator;
+
+class AuthController extends Controller
+{
+    public function index () {
+        return view('auth.login');
+    }
+
+    public function login(Request $request) {
+        $validation = Validator::make($request->all(), [
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+
+        if($validation->fails()) {
+            return back()->withErrors("message", "Data tidak lengkap");
+        }
+
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors("message", "Gagal login");
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect('/');
+    }
+}
+
+?>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Http\Controllers\Auth\AuthController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PekerjaController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\ClientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,13 +16,8 @@ use App\Http\Controllers\PekerjaController;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
-// Route::get('index', function () {
-//     return view('index');
-// });
-// Route::get('/', function () {
-//     return view('index');
-// });
+Route::get('/', [AuthController::class, 'index'])->name("login-view");
+Route::post('/login-account', [AuthController::class, 'login'])->name("login-account");
 // Route::get('formclient', function () {
 //     return view('formclient');
 // });
@@ -29,7 +27,7 @@ Route::get('invoice', function () {
 Route::get('forminvoice', function () {
     return view('forminvoice');
 });
-Route::post('/insertdata',[InvoiceController::class, 'store'])->name('insertdata'); 
+Route::post('/insertdata',[InvoiceController::class, 'store'])->name('insertdata');
 Route::resource('pekerja', PekerjaController::class);
 Route::resource('client', ClientController::class);
 // Route::get('formpekerja', function (){
